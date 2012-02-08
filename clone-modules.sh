27acd3004b63667fbfb98887a3615b585f0755b6
Olivier Grisel <olivier.grisel@ensta.org>
2012-02-08 12:08:41 +0100

    make it easy to fetch and build all project at once

---
+++ clone-modules.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+set +ex
+
+if [ ! -d stanbol-temis ]
+then
+    git clone https://github.com/nuxeo/stanbol-temis
+fi
+if [ ! -d stanbol-mondeca ]
+then
+    git clone https://github.com/nuxeo/stanbol-mondeca
+fi
+if [ ! -d nuxeo-platform-semantic-entities ]
+then
+    git clone https://github.com/nuxeo/nuxeo-platform-semantic-entities
+fi
